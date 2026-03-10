ce82fd623c0d4b3fbeae06f6df3b0605d658dc6f
Yuchen Li <yrcli@ucdavis.edu>
2026-03-10 15:24:10 -0700

    add argument and file validation

--- vcf_counter.sh
+++ vcf_counter.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+VCF_FILE="$1"
+
+#test to see if the file is provided
+if [ -z "$VCF_FILE" ]; then
+    echo "Error: please provide a VCF file"
+    exit 1
+fi
+
+#test to see if the file exist
+if [ ! -f "$VCF_FILE" ]; then
+    echo "Error: File '$VCF_FILE' not found"
+    exit 1
+fi
+
+#test to see if the file format is correct
+if [[ "$VCF_FILE" != *.vcf ]]; then
+    echo "Error: File must end in .vcf"
+    exit 1
+fi
+
